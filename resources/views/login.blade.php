d931d5adee2437cad5fefccfd7ba3ef2d7ab85ab
TheLadsters <jeraldmags@gmail.com>
2022-09-16 21:09:16 +0800

    Added fire-track laravel

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>FireTrack App</title>
+</head>
+
+<body>
+    <h1>FireTrack App</h1>
+    LOGIN PAGE
+    <a href="layout"><button>LOGIN</button></a>
+</body>
+
+
+</html>
