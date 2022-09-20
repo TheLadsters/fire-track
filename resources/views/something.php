a96709f57a918c9cafdc5cf7f65295cf16a916cb
TheLadsters <jeraldmags@gmail.com>
2022-09-20 23:15:03 +0800

    deleted something.php. rendered useless

--- resources/views/something.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar navbar-expand-lg" style="background-color: #C0371B;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-        <img src="images/firetracklogo.png" alt="FireTrack" width="100" height="24" id="firetrack_logo">
-    </a>
-  </div>
-</nav>
