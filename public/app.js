17462cdf49cd129e292839b52828ddc44f456327
TheLadsters <jeraldmags@gmail.com>
2022-09-26 17:41:21 +0800

    added js for edit profile features

--- public/app.js
+++ public/app.js
@@ -14,6 +14,8 @@ $('.close-btn').on('click', function(){
 });
 // end of code
 
+
+
 // forgot password modal start
 $('#forgotPassword').on('click', function () {
     $('#forgetPasswordModal').modal('show');
@@ -26,5 +28,106 @@ $('#dismissX').on('click', function () {
 $('#forgotPassword').on('click', function () {
     $('#forgetPasswordModal').modal('show');
 }) 
-
 // end of forgot password modal code
+
+
+
+// start of editprofile code
+
+// code for hiding/unhiding passwords
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(e) {
+        e.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "bi bi-eye-slash" );
+            $('#show_hide_password i').removeClass( "bi bi-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "bi bi-eye-slash" );
+            $('#show_hide_password i').addClass( "bi bi-eye" );
+        }
+    });
+
+    $("#show_hide_current_password a").on('click', function(e) {
+        e.preventDefault();
+        if($('#show_hide_current_password input').attr("type") == "text"){
+            $('#show_hide_current_password input').attr('type', 'password');
+            $('#show_hide_current_password i').addClass( "bi bi-eye-slash" );
+            $('#show_hide_current_password i').removeClass( "bi bi-eye" );
+        }else if($('#show_hide_current_password input').attr("type") == "password"){
+            $('#show_hide_current_password input').attr('type', 'text');
+            $('#show_hide_current_password i').removeClass( "bi bi-eye-slash" );
+            $('#show_hide_current_password i').addClass( "bi bi-eye" );
+        }
+    });
+
+    $("#show_hide_new_password a").on('click', function(e) {
+        e.preventDefault();
+        if($('#show_hide_new_password input').attr("type") == "text"){
+            $('#show_hide_new_password input').attr('type', 'password');
+            $('#show_hide_new_password i').addClass( "bi bi-eye-slash" );
+            $('#show_hide_new_password i').removeClass( "bi bi-eye" );
+        }else if($('#show_hide_new_password input').attr("type") == "password"){
+            $('#show_hide_new_password input').attr('type', 'text');
+            $('#show_hide_new_password i').removeClass( "bi bi-eye-slash" );
+            $('#show_hide_new_password i').addClass( "bi bi-eye" );
+        }
+    });
+
+
+    $("#show_hide_repeat_new_password a").on('click', function(e) {
+        e.preventDefault();
+        if($('#show_hide_repeat_new_password input').attr("type") == "text"){
+            $('#show_hide_repeat_new_password input').attr('type', 'password');
+            $('#show_hide_repeat_new_password i').addClass( "bi bi-eye-slash" );
+            $('#show_hide_repeat_new_password i').removeClass( "bi bi-eye" );
+        }else if($('#show_hide_repeat_new_password input').attr("type") == "password"){
+            $('#show_hide_repeat_new_password input').attr('type', 'text');
+            $('#show_hide_repeat_new_password i').removeClass( "bi bi-eye-slash" );
+            $('#show_hide_repeat_new_password i').addClass( "bi bi-eye" );
+        }
+    });
+});
+
+// SHOW UPLOADED IMAGE
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult')
+                .attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    });
+});
+// END OF UPLOAD IMG
+
+$('#resetPasswordBtn').on('click', function () {
+    $('#changePasswordModal').modal('show');
+  })
+
+  $('#dismissX').on('click', function () {
+    $('#changePasswordModal').modal('hide');
+  })
+
+  $('#dismissX2').on('click', function () {
+    $('#forgetPasswordModal').modal('hide');
+  })
+
+  $('#forgotPassLink').on('click', function () {
+    $('#changePasswordModal').modal('hide');
+    $('#forgetPasswordModal').modal('show');
+  })  
+
+
+// end of editprofile code
+
+
