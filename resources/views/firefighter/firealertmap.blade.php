78ee266ee08b8fe0aedd2846a3e9faeedc2dcc74
TheLadsters <jeraldmags@gmail.com>
2022-09-26 20:53:00 +0800

    added respective blades for firealert map and firehydrant map

---
+++ resources/views/firefighter/firealertmap.blade.php
@@ -0,0 +1,5 @@
+@extends('/layout')
+
+@section('fireAlertMap')
+<img src="./images/maptemplate.png" width="100%" height="100%"/>
+@endsection
---
+++ resources/views/firefighter/firehydrantmap.blade.php
@@ -0,0 +1,5 @@
+@extends('/layout')
+
+@section('fireHydrantMap')
+<img src="./images/maptemplate.png" width="100%" height="100%"/>
+@endsection
