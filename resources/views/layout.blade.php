b76f47b7630021da6a3f819f4825402d98e85ded
TheLadsters <jeraldmags@gmail.com>
2022-09-26 17:45:04 +0800

    added the bootstrap icon cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/app.css">
     <title>FireTrack App</title>
 </head>
@@ -66,7 +67,7 @@
           </div>
 
       </div>
-
+    </div>
 
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
