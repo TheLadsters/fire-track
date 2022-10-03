f60f754e50f1af1b8a23c5a9e5c43ee2dfb39412
TheLadsters <jeraldmags@gmail.com>
2022-10-03 18:40:44 +0800

    added id to body body-login, added new google font roboto, added poppermin js

--- resources/views/signlayout.blade.php
+++ resources/views/signlayout.blade.php
@@ -8,6 +8,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,36 +17,14 @@
     <title>FireTrack App</title>
 </head>
 
-<body>
-
-          <nav class="navbar navbar-expand-md navbar-light" style="background-color: #C0371B;">
-            <div class="container-fluid">
-              <div class="d-flex justify-content-between d-md-none d-block">
-                  <a class="navbar-brand fs-4" href="#">
-                    <img src="images/firetracklogo.png" alt="FireTrack" width="100" height="24" id="firetrack_logo">
-                  </a>
-              </div>
-              <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
-                      <img src="images/firetracklogo.png" alt="FireTrack" width="100" height="24" id="firetrack_logo">
-                    </a>
-                  </li>
-                 
-                </ul>
-              </div>
-            </div>
-          </nav>
+<body id="body-login">
 
           
-          <div class="main-content">
             {{-- VIEW OUTPUT HERE --}}
 
             {{-- login view --}}
             @yield('loginsection')
             @yield('signuppage')
-          </div>
 
 
 
@@ -53,6 +32,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
       integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
       <script type="text/javascript" src="/app.js"></script>
 </body>
 
