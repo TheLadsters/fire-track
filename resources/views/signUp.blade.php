b0258e895d3300b60a6f76a45a956bfbd0ce0d2a
TheLadsters <jeraldmags@gmail.com>
2022-10-03 22:08:54 +0800

    changed UI

--- resources/views/signUp.blade.php
+++ resources/views/signUp.blade.php
@@ -1,87 +1,97 @@
-@extends('signlayout')
+@extends('signUpLayout')
 
-@section('signuppage')
+@section('signup-page')
 
-<div class="signup-body">
-    
-    <div class="signup">
+<section class="h-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-8 col-xl-6">
+        <div class="card rounded-5">
+          <div class="row d-flex justify-content-center mt-3">
+          <img src= "/images/fire_track_logo_black.png"
+            class="fireTrackBlack" alt="Fire Track Logo">
+          </div>
 
-        <div class="text-center">
-            <h1 id="signup-title">Sign Up</h1>
-            <p><b>User Registration</b></p>
-        </div>
-        
-        <form class="row">
-            <div class="row">
-                <div class="col-md-12 mb-2">
-                    <a href="/">
-                        <button type="button" class="btn backtologin">
-                            <i class="fa-solid fa-backward"></i> Back to Login
-                        </button>
-                    </a>
+          <div class="card-body p-4 p-md-5">
+            <h3 class="text-center pb-2 pb-md-0 mb-md-5 px-md-2"><b>CREATE AN ACCOUNT</b></h3>
+
+            <form class="px-md-2">
+
+              <div class="row">
+                <div class="col-md-6 mb-4">
+                    <input type="text" class="form-control" id="firstNameInput" placeholder="First Name" />
                 </div>
-            </div>
 
-            <div class="col-md-6 form-group py-1">
-                <input class="form-control inputForm" type="text" placeholder="First Name">
-            </div>
-            <div class="col-md-6 form-group py-1">
-                <input class="form-control inputForm" type="text" placeholder="Last Name">
-            </div>
-
-            <div class="row">
-                <div class="col-md-12 py-2">
-                    <input class="form-control inputForm" type="text" placeholder="Email or mobile number">
+                <div class="col-md-6 mb-4">
+                  <input type="text" class="form-control" id="firstNameInput" placeholder="Last Name" />
                 </div>
-            </div>
+              </div>
 
-            <div class="row">
-                <div class="col-md-12 py-2">
-                    <input class="form-control inputForm" type="password" placeholder="Password">
+              <div class="row">
+                <div class="col-md-6 mb-4">
+                    <input type="text" class="form-control" id="contactNumberInput" placeholder="Contact Number" />
                 </div>
-            </div>
 
-            <div class="row">
-                <div class="col-md-12 py-2">
-                    <input class="form-control inputForm" type="text" placeholder="Address Line 1">
+                <div class="col-md-6 mb-4">
+                  <input type="text" class="form-control" id="emailAddressInput" placeholder="Email Address" />
                 </div>
-            </div>
+              </div>
 
-            <div class="row">
-                <div class="col-md-12 py-2">
-                    <input class="form-control inputForm" type="text" placeholder="Address Line 2">
+  
+
+              <div class="row mb-4 pb-2 pb-md-0 mb-md-3">
+                <div class="col-md-12 mb-4">
+                    <input type="text" id="homeAddressInput" class="form-control" placeholder="Home Address" />
                 </div>
-            </div>
 
-            <div class="row mt-3">
-                <h4>Gender</h4>
-                <div class="col-md-6 genderSelect">
-                    <input class="form-check-input" type="radio" name="gender" id="maleInput">
-                    <label class="form-check-label" for="maleInput">
-                        Male
-                      </label>
-                </div>
-                <div class="col-md-6 genderSelect">
-                    <input class="form-check-input" type="radio" name="gender" id="femaleInput">
-                    <label class="form-check-label" for="femaleInput">
-                        Female
-                      </label>
-                </div>
-            </div>
-
-            <p id="termspolicy" class="py-2 pt-3">
-                By clicking Sign Up, you agree to our Terms, Data Policy and Cookies Policy. 
-                You may receive Email Notifications from us and can opt out any time.
-            </p>
-
-            <div class="row">
                 <div class="col-md-12">
-                    <a href="/"><input id="signUpBtn" class="w-100" type="button" value="SIGN UP"></a>
+                    <input type="password" id="passwordInput" class="form-control" placeholder="Password" />
                 </div>
-            </div>
-        </form>
-    </div>
+              </div>
 
-</div>
+              <div class="row mb-3 mt-4 genderArea">
+                <div class="col-md-2 mb-2">
+                  Gender
+                </div>
+
+                <div class="col-md-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="chooseGender" id="genderMale">
+                    <label class="form-check-label" for="chooseGender">
+                      Male
+                    </label>
+                  </div>
+                </div>
+
+                <div class="col-md-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="radio" name="chooseGender" id="genderFemale">
+                    <label class="form-check-label" for="chooseGender">
+                      Female
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <p class="text-center">
+                    By clicking Sign Up, you agree to our Terms, Data Policy and Cookies Policy. 
+                    You may receive Email Notifications from us and can opt out any time.
+                  </p>
+                </div>
+              </div>
+
+
+              <button class="btn-signUp btn-block-signup btn-lg mb-1">Sign Up</button>
+
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 @endsection
