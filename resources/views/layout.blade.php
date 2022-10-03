9e807adf79e1d9a87883b3263c1db606a605ad18
TheLadsters <jeraldmags@gmail.com>
2022-10-03 18:43:17 +0800

    added new sidebar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;800&display=swap" rel="stylesheet">
@@ -18,59 +19,49 @@
 </head>
 
 <body id="body-pd">
-
-
-      <div class="main-container d-flex">
-        <div class="sidebar" id="side_nav">
-          <div class="header-box px-2 pt-4 pb-2 d-flex justify-content-between">
-              <h1 class="fs-4"><span class="px-2 me-2"><i class="fas fa-user"></i></span><span style="color:#C0371B;">John Doe</span></h1>
-              <button class="btn d-md-none d-block close-btn px-2 py-0"><i class="fas fa-bars"></i></button>
-          </div>
-          
-          <hr class="h-color mx-2">
-
-          <ul class="list-unstyled px-2">
-            <li class=""><a href="/edit-profile" class="text-decoration-none d-block"><i class="fas fa-user-edit"></i>Edit Profile</a></li>
-            <li class=""><a href="/fire-hydrant-map" class="text-decoration-none d-block"><i class="fas fa-map-marker-alt"></i>Fire Hydrant Map</a></li>
-            <li class=""><a href="/fire-alert-map" class="text-decoration-none d-block"><i class="fa-solid fa-fire"></i>Fire Alert Map</a></li>
-            <li class=""><a href="#" class="text-decoration-none d-block"><i class="fa-regular fa-file"></i>Generate Report</a></li>
-            <li class=""><a href="#" class="text-decoration-none d-block"><i class="fa-regular fa-newspaper"></i>Bulletin</a></li>
-            <li class=""><a href="/" class="text-decoration-none d-block"><i class="fa-solid fa-right-from-bracket"></i>Logout</a></li>
-          </ul>
-        </div>
-
-        <div class="content">
-
-          <nav class="navbar navbar-expand-md navbar-light" style="background-color: #C0371B;">
-            <div class="container-fluid">
-              <div class="d-flex justify-content-between d-md-none d-block">
-                <button class="btn px-2 py-0 open-btn me-2"><i class="fas fa-bars"></i></button>
-                  <a class="navbar-brand fs-4" href="#">
-                    <img src="images/firetracklogo.png" alt="FireTrack" width="100" height="24" id="firetrack_logo">
-                  </a>
-              </div>
-              <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
-                      <img src="images/firetracklogo.png" alt="FireTrack" width="100" height="24" id="firetrack_logo">
-                    </a>
-                  </li>
-                 
-                </ul>
-              </div>
-            </div>
-          </nav>
-
-          <div class="main-content px-3 pt-4">
-            @yield('editProfile')
-            @yield('fireAlertMap')
-            @yield('fireHydrantMap')
-          </div>
-
-      </div>
+    <header class="header" id="header">
+        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="head_logo"> <img src="images/fire_track_logo_black.png" alt="fire track logo"> </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div> <a href="#" class="nav_logo"> <i class='bx bx-user nav_logo-icon'></i> <span class="nav_user-name">John Doe</span> </a>
+                <div class="nav_list"> 
+                  <a href="#" class="nav_link active"> 
+                    <i class='bx bx-user-circle nav_icon'></i>
+                    <span class="nav_name">Edit Profile</span> 
+                  </a> 
+                  
+                  <a href="#" class="nav_link"> 
+                    <i class='bx bx-map-alt nav_icon'></i>
+                    <span class="nav_name">Fire Hydrant Map</span> 
+                  </a> 
+                  
+                  <a href="#" class="nav_link"> 
+                    <i class='bx bxs-hot nav_icon'></i> 
+                    <span class="nav_name">Fire Alert Map</span> 
+                  </a> 
+                  
+                  <a href="#" class="nav_link"> 
+                    <i class='bx bxs-report' ></i>
+                    <span class="nav_name">Generate Reports</span> 
+                  </a> 
+                  
+                  <a href="#" class="nav_link"> 
+                    <i class='bx bx-news nav_icon'></i>
+                    <span class="nav_name">Bulletin</span> 
+                  </a> 
+                  
+                </div>
+            </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">Sign Out</span> </a>
+        </nav>
     </div>
-
+    <!--Container Main start-->
+    <div class="height-100 bg-light">
+        <h4>Main Components</h4>
+        @yield('editProfile')
+    </div>
+    <!--Container Main end-->
 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
       integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
