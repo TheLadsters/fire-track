7947894545d4000ccaa894ab85f9a83db701d70b
TheLadsters <jeraldmags@gmail.com>
2022-11-02 22:21:36 +0800

    commented out header links in edit profile

--- resources/views/firefighter/editprofile.blade.php
+++ resources/views/firefighter/editprofile.blade.php
@@ -1,7 +1,7 @@
 @extends('firefighter/layoutFirefighter')
 
 @section('editProfile')
-<head>
+{{-- <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
@@ -13,71 +13,77 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head> --}}
+<head>
+    <link rel="stylesheet" href="css/editprofile-firefighter.css">
 </head>
 
-<div class="container-xl px-4 mt-4">
-    <hr class="mt-0 mb-4">
-    <div class="row">
-        <div class="col-xl-4">
-            <!-- Profile picture card-->
-            <div class="card mb-4 mb-xl-0">
-                <div class="card-header">Profile Picture</div>
-                <div class="card-body text-center">
-                    <!-- Profile picture image-->
-                    <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
-                    <!-- Profile picture help block-->
-                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                    <!-- Profile picture upload button-->
-                    <div class="form-group">
-						<label>Upload new image</label>
-						<input type="file" class="form-control" required>
-					</div>	
+
+<div id="body">
+    <div class="container-xl px-4 mt-4">
+        <hr class="mt-0 mb-4">
+        <div class="row">
+            <div class="col-xl-4">
+                <!-- Profile picture card-->
+                <div class="card mb-4 mb-xl-0">
+                    <div class="card-header">Profile Picture</div>
+                    <div class="card-body text-center">
+                        <!-- Profile picture image-->
+                        <img class="img-account-profile rounded-circle mb-2" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+                        <!-- Profile picture help block-->
+                        <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                        <!-- Profile picture upload button-->
+                        <div class="form-group">
+                            <label>Upload new image</label>
+                            <input type="file" class="form-control" required>
+                        </div>	
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-xl-8">
-            <!-- Account details card-->
-            <div class="card mb-4">
-                <div class="card-header">Account Details</div>
-                <div class="card-body">
-                    <form>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3">
-                            <!-- Form Group (first name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputFirstName">First name</label>
-                                <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" >
+            <div class="col-xl-8">
+                <!-- Account details card-->
+                <div class="card mb-4">
+                    <div class="card-header">Account Details</div>
+                    <div class="card-body">
+                        <form>
+                            <!-- Form Row-->
+                            <div class="row gx-3 mb-3">
+                                <!-- Form Group (first name)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputFirstName">First name</label>
+                                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" >
+                                </div>
+                                <!-- Form Group (last name)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputLastName">Last name</label>
+                                    <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name">
+                                </div>
                             </div>
-                            <!-- Form Group (last name)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputLastName">Last name</label>
-                                <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name">
+                            <!-- Form Group (Address)-->
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputAddress">Address</label>
+                                <input class="form-control" id="inputAddress" type="text" placeholder="Enter your address">
                             </div>
-                        </div>
-                        <!-- Form Group (Address)-->
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputAddress">Address</label>
-                            <input class="form-control" id="inputAddress" type="text" placeholder="Enter your address">
-                        </div>
-                        <!-- Form Row-->
-                        <div class="row gx-3 mb-3   ">
-                            <!-- Form Group (phone number)-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputPhone">Phone number</label>
-                                <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number">
+                            <!-- Form Row-->
+                            <div class="row gx-3 mb-3   ">
+                                <!-- Form Group (phone number)-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputPhone">Phone number</label>
+                                    <input class="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number">
+                                </div>
+                                <!-- Form Group (Email address-->
+                                <div class="col-md-6">
+                                    <label class="small mb-1" for="inputEmail">Email Address</label>
+                                    <input class="form-control" id="inputEmail" type="text" name="email address" placeholder="Enter your email address">
+                                </div>
+                                <div class="col-md-6">
+                                    <br><a href="#ChangePasswordModal" id="changePass"><label class="small mb-1">Change Password</label></a>
+                                </div>
                             </div>
-                            <!-- Form Group (Email address-->
-                            <div class="col-md-6">
-                                <label class="small mb-1" for="inputEmail">Email Address</label>
-                                <input class="form-control" id="inputEmail type="text" name="email address" placeholder="Enter your email address">
-                            </div>
-                            <div class="col-md-6">
-                                <br><a href="#ChangePasswordModal" class="edit" data-toggle="modal"><label class="small mb-1">Change Password</label></a>
-                            </div>
-                        </div>
-                        <!-- Save changes button-->
-                        <button class="btn btn-primary" type="button">Save changes</button>
-                    </form>
+                            <!-- Save changes button-->
+                            <button class="btn btn-primary" type="button">Save changes</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -116,67 +122,5 @@
 	</div>
 </div>
 
-<style>
-    body{margin-top:20px;
-background-color:#f2f6fc;
-color:#69707a;
-margin:190px;
-}
-.img-account-profile {
-    height: 10rem;
-}
-.rounded-circle {
-    border-radius: 50% !important;
-}
-.card {
-    box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
-}
-.card .card-header {
-    font-weight: 500;
-}
-.card-header:first-child {
-    border-radius: 0.35rem 0.35rem 0 0;
-}
-.card-header {
-    padding: 1rem 1.35rem;
-    margin-bottom: 0;
-    background-color: rgba(33, 40, 50, 0.03);
-    border-bottom: 1px solid rgba(33, 40, 50, 0.125);
-}
-.form-control, .dataTable-input {
-    display: block;
-    width: 100%;
-    padding: 0.875rem 1.125rem;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1;
-    color: #69707a;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #c5ccd6;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    border-radius: 0.35rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-}
-
-.nav-borders .nav-link.active {
-    color: #0061f2;
-    border-bottom-color: #0061f2;
-}
-.nav-borders .nav-link {
-    color: #69707a;
-    border-bottom-width: 0.125rem;
-    border-bottom-style: solid;
-    border-bottom-color: transparent;
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-    padding-left: 0;
-    padding-right: 0;
-    margin-left: 1rem;
-    margin-right: 1rem;
-}
-</style>
 
 @endsection
