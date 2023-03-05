f7467d023825ad363575b586edb4e58f9197c7f4
evodario <19103225@usc.edu.ph>
2023-03-05 12:02:57 +0800

    Initial Bulletin

--- public/bulletinNews.js
+++ public/bulletinNews.js
@@ -22,11 +22,34 @@ function retrieve(e){
         console.log(data)
         data.articles.forEach(article =>{
             let li = document.createElement('li');
-            let a = document.createElement('a');
+            let a = document.createElement('p');
+            let img = document.createElement("img");
+
+            li.style.display = "block";
+            li.style.width = "90%";
+            li.style.height = "20%";
+            li.style.padding = "10px";
+            li.style.border = "1px solid blue";
+            li.style.borderRadius = "15px";
+            li.style.marginTop = "20px";
+            li.style.backgroundImage= "url(article.urlToImage)";
+            
+
+            img.src = article.urlToImage;
+            // img.style.float = "left";
+            img.style.border = "10px solid orange";
+            img.style.width = "250px";
+            img.style.height = "150px";
+
+            li.appendChild(img);
+
+            a.style.marginLeft = "20%";
+
             a.setAttribute('href', article.url);
             a.setAttribute('target', '_blank');
             a.textContent = article.title;
             li.appendChild(a);
+            
             newsList.appendChild(li);
         })
 
