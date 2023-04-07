8e2428aac3db42c21a86c582b7b96413de4594cc
Jerald Blaise Magsipoc <19100414@usc.edu.ph>
2023-04-08 02:19:31 +0800

    Added admin details

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();        
+        \App\Models\User::factory(1)->create();    
+        
+        DB::table('users')->insert([
+            'email' => 'firetrackCapstone@gmail.com',
+            'fname' => 'admin',
+            'lname' => 'admin',
+            'contact_no' => '12345678',
+            'password' => 'admin123',
+            'birthday' => '04082000',
+            'gender' => 'male',
+            'address' => 'Cebu City, Philippines',
+            'img_url' => null,
+            'role' => 'admin',
+            'status' => 'active',
+        ]);
     }
 }
