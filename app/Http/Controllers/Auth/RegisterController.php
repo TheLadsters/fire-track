d76283988aff555f69cb4d2d3c443b7448212932
Jerald Blaise Magsipoc <jeraldmags@gmail.com>
2023-11-21 23:06:16 +0800

    removed birthday in register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -99,7 +99,6 @@ class RegisterController extends Controller
          $user->contact_no = $request->contact_no;
          $user->gender = $request->gender;
          $user->role = 'firefighter';
-         $user->birthday = 03102001;
          $user->address = $request->address;
          $user->img_url = NULL;
          $user->password = \Hash::make($request->password);
